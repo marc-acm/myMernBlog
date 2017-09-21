f88aada69992b99f6b9e41debe76eba666f5de74
marc-acm <marc.macariola@yahoo.com>
2017-09-21 11:52:08 +0800

    initial commit

--- dist/server.bundle.js
+++ dist/server.bundle.js
@@ -255,7 +255,7 @@
 	  value: true
 	});
 	var config = {
-	  mongoURL: process.env.MONGO_URL || 'mongodb://marc-acm:Q1V74DFkmlVhhZLu@cluster0-shard-00-00-eob5s.mongodb.net:27017,cluster0-shard-00-01-eob5s.mongodb.net:27017,cluster0-shard-00-02-eob5s.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+	  mongoURL: process.env.MONGO_URL || 'mongodb://marc-acm:qciO7efG3Rgki9HR@cluster0-shard-00-00-eob5s.mongodb.net:27017,cluster0-shard-00-01-eob5s.mongodb.net:27017,cluster0-shard-00-02-eob5s.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
 	  port: process.env.PORT || 8000
 	};
 	
